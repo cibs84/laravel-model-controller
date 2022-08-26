c488b925f1b48085d4fca285d7dd072d1e8ea0dc
cibs84 <d.tilesi@gmail.com>
2022-08-27 01:26:34 +0200

    styling done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,9 +12,50 @@ class HomeController extends Controller
         $movies = Movie::all();
 
         $data = [
-            'movies' => $movies
+            'movies' => $movies,
+            'posters' => [
+                [
+                    'id' => 1,
+                    'url' => 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/r4gnMXoY1efvaolNDjn3nj4046S.jpg'
+                ],
+                [
+                    'id' => 2,
+                    'url' => 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xRyW60TXvX7Q2HSbpz8nZxKaTkL.jpg'
+                ],
+                [
+                    'id' => 3,
+                    'url' => 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/tdqX0MWaFHuGwUygYn7j6eluOdP.jpg'
+                ],
+                [
+                    'id' => 4,
+                    'url' => 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4pRXqT7wQmoH5sH6Z4WYmbAFG4t.jpg'
+                ],
+                [
+                    'id' => 5,
+                    'url' => 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mGnOtBmpkQ5CndwxeIKDUqkUkre.jpg'
+                ],
+                [
+                    'id' => 6,
+                    'url' => 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9p10J9Xp7MuaVac56g8BwAuXEsA.jpg'
+                ],
+                [
+                    'id' => 7,
+                    'url' => 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pGZqc9jQ6F8klQZKm37NAAMH70o.jpg'
+                ],
+                [
+                    'id' => 8,
+                    'url' => 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fVqKwpvobwWy0P1UImZWIDuw4RI.jpg'
+                ],
+                [
+                    'id' => 9,
+                    'url' => 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qKMqJCldp5fPsuv6Vb0RbKY9K8U.jpg'
+                ],
+                [
+                    'id' => 10,
+                    'url' => 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8P4fZhbiqfeMk2btOeXLu0AKvBl.jpg'
+                ],
+            ]
         ];
-
         return view('homepage', $data);
     }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,15 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-    @foreach ($movies as $movie)
-        <div class="title">Title: {{$movie->title}}</div>
-        <div class="original-title">Original Title: {{$movie->original_title}}</div>
-        <div class="nationality">Nationality: {{$movie->nationality}}</div>
-        <div class="date">Date: {{$movie->date}}</div>
-        <div class="vote">Vote: {{$movie->vote}}</div>
-        <hr>
-    @endforeach
+    {{-- Heading --}}
+    <h1>I Film più premiati</h1>
+    
+    <div class="container">
+        
+        {{-- Movie List --}}
+        @for ($i = 0; $i < count($movies); $i++)
+            {{-- Card --}}
+            <div class="card">
+                <img src="{{ $posters[$i]['url'] }}" alt="">
+
+                <div>
+                    <h2 class="title">{{$movies[$i]->title}}</h2>
+                    <h3 class="original-title">({{$movies[$i]->original_title}})</h3>
+                    <div class="nationality"><span>Nazionalità: </span>{{$movies[$i]->nationality}}</div>
+                    <div class="date"><span>Data: </span>{{$movies[$i]->date}}</div>
+                    <div class="vote"><span>Voto: </span>{{$movies[$i]->vote}}</div>
+                </div>
+            </div>
+        @endfor
+    </div>
 </body>
 </html>
